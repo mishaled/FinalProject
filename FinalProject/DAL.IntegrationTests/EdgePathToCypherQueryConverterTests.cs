50929bb8a57833cc2eaa9197d1e1fd10e86bf0cf
Misha <mishaled@gmail.com>
2017-08-26 15:05:00 +0300

    compute query from path

---
+++ FinalProject/DAL.IntegrationTests/EdgePathToCypherQueryConverterTests.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+
+namespace DAL.IntegrationTests
+{
+    [TestClass]
+    public class EdgePathToCypherQueryConverterTests
+    {
+        private TestContext testContextInstance;
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [TestMethod]
+        public void Convert_OneEdge_ShouldConvertCorrectly()
+        {
+            List<DFS_Code> path = new List<DFS_Code>();
+            path.Add(new DFS_Code()
+            {
+                u = 1,
+                l_u = 1,
+                v = 2,
+                l_v = 2,
+                l_w = 1
+            });
+
+            var converter = new EdgePathToCypherQueryConverter();
+            var query = converter.Convert(path);
+
+            Assert.AreEqual(query, "match(n0:Node { id: 1, label: 1})-[:CONNECTED_TO {label: 1 }]-(n1 {id: 2, label: 2}) return DISTINCT n0.graphId");
+        }
+
+        [TestMethod]
+        public void Convert_TwoEdgesInCorrectOrder_ShouldConvertCorrectly()
+        {
+            List<DFS_Code> path = new List<DFS_Code>();
+            path.Add(new DFS_Code()
+            {
+                u = 1,
+                l_u = 1,
+                v = 2,
+                l_v = 2,
+                l_w = 1
+            });
+
+            path.Add(new DFS_Code()
+            {
+                u = 2,
+                l_u = 2,
+                v = 3,
+                l_v = 3,
+                l_w = 2
+            });
+
+            var converter = new EdgePathToCypherQueryConverter();
+            var query = converter.Convert(path);
+
+            Assert.AreEqual(query, "match(n0:Node { id: 1, label: 1})-[:CONNECTED_TO {label: 1 }]-(n1 {id: 2, label: 2})-[:CONNECTED_TO {label: 2 }]-(n2 {id: 3, label: 3}) return DISTINCT n0.graphId");
+        }
+
+        [TestMethod]
+        public void Convert_TwoEdgesWhenTheFirstIsReversed_ShouldConvertCorrectly()
+        {
+            List<DFS_Code> path = new List<DFS_Code>();
+            path.Add(new DFS_Code()
+            {
+                u = 2,
+                l_u = 2,
+                v = 1,
+                l_v = 1,
+                l_w=1
+            });
+
+            path.Add(new DFS_Code()
+            {
+                u = 2,
+                l_u = 2,
+                v = 3,
+                l_v = 3,
+                l_w = 2
+            });
+
+            var converter = new EdgePathToCypherQueryConverter();
+            var query = converter.Convert(path);
+
+            Assert.AreEqual(query, "match(n0:Node { id: 1, label: 1})-[:CONNECTED_TO {label: 1 }]-(n1 {id: 2, label: 2})-[:CONNECTED_TO {label: 2 }]-(n2 {id: 3, label: 3}) return DISTINCT n0.graphId");
+        }
+
+        [TestMethod]
+        public void Convert_TwoEdgesWhenTheSecondIsReversed_ShouldConvertCorrectly()
+        {
+            List<DFS_Code> path = new List<DFS_Code>();
+            path.Add(new DFS_Code()
+            {
+                u = 1,
+                l_u = 1,
+                v = 2,
+                l_v = 2,
+                l_w = 1
+            });
+
+            path.Add(new DFS_Code()
+            {
+                u = 3,
+                l_u = 3,
+                v = 2,
+                l_v = 2,
+                l_w = 2
+            });
+
+            var converter = new EdgePathToCypherQueryConverter();
+            var query = converter.Convert(path);
+
+            Assert.AreEqual(query, "match(n0:Node { id: 1, label: 1})-[:CONNECTED_TO {label: 1 }]-(n1 {id: 2, label: 2})-[:CONNECTED_TO {label: 2 }]-(n2 {id: 3, label: 3}) return DISTINCT n0.graphId");
+        }
+
+        [TestMethod]
+        public void Convert_TwoEdgesWhenBothIsReversed_ShouldConvertCorrectly()
+        {
+            List<DFS_Code> path = new List<DFS_Code>();
+            path.Add(new DFS_Code()
+            {
+                u = 2,
+                l_u = 2,
+                v = 1,
+                l_v = 1,
+                l_w=1
+            });
+
+            path.Add(new DFS_Code()
+            {
+                u = 3,
+                l_u = 3,
+                v = 2,
+                l_v = 2,
+                l_w = 2
+            });
+
+            var converter = new EdgePathToCypherQueryConverter();
+            var query = converter.Convert(path);
+
+            Assert.AreEqual(query, "match(n0:Node { id: 1, label: 1})-[:CONNECTED_TO {label: 1 }]-(n1 {id: 2, label: 2})-[:CONNECTED_TO {label: 2 }]-(n2 {id: 3, label: 3}) return DISTINCT n0.graphId");
+        }
+    }
+}
---
+++ FinalProject/DAL/EdgePathToCypherQueryConverter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace DAL
+{
+    public class EdgePathToCypherQueryConverter
+    {
+        private int nextId;
+        private int nextLabel;
+
+        public string Convert(List<DFS_Code> edges)
+        {
+            string query = string.Empty;
+
+            if (edges.Count == 0)
+            {
+                return query;
+            }
+
+            for (var index = 0; index < edges.Count; index++)
+            {
+                DFS_Code edge = edges[index];
+                Tuple<int, int, int, int> order = ComputeEdgeOrder(edges, index, edge);
+
+                if (index == 0)
+                {
+                    query += string.Format("match(n{0}:Node {{ id: {1}, label: {2}}})",index, order.Item1, order.Item2);
+                }
+
+                query += string.Format("-[:CONNECTED_TO {{label: {0} }}]-(n{1} {{id: {2}, label: {3}}})", edge.l_w, index + 1, order.Item3, order.Item4);
+            }
+
+            query += " return DISTINCT n0.graphId";
+
+            return query;
+        }
+
+        private Tuple<int, int, int, int> ComputeEdgeOrder(List<DFS_Code> edges, int index, DFS_Code edge)
+        {
+            Tuple<int, int, int, int> order = null;
+            if (index + 1 < edges.Count)
+            {
+                order = ComputeEdgeOrder(edge, edges[index + 1]);
+            }
+            else if (index > 0)
+            {
+                Tuple<int, int, int, int> reversed = ComputeEdgeOrder(edges[index], edges[index - 1]);
+                order = Tuple.Create(reversed.Item3, reversed.Item4, reversed.Item1, reversed.Item2);
+            }
+            else
+            {
+                order = Tuple.Create(edge.u, edge.l_u, edge.v, edge.l_v);
+            }
+            return order;
+        }
+
+        private Tuple<int, int, int, int> ComputeEdgeOrder(DFS_Code firstEdge, DFS_Code secondEdge)
+        {
+            if (firstEdge.u == secondEdge.u || firstEdge.u == secondEdge.v)
+            {
+                return new Tuple<int, int, int, int>(firstEdge.v, firstEdge.l_v, firstEdge.u, firstEdge.l_u);
+            }
+
+            return new Tuple<int, int, int, int>(firstEdge.u, firstEdge.l_u, firstEdge.v, firstEdge.l_v);
+        }
+    }
+}

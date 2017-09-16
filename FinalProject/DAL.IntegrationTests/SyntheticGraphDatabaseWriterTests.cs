87533027aa6544326bba8fe74b075a1421327f08
Misha <mishaled@gmail.com>
2017-09-16 17:37:50 +0300

    Synth file writer

---
+++ FinalProject/DAL.IntegrationTests/SyntheticGraphDatabaseWriterTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.IO;
+using DAL.IntegrationTests.Properties;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+
+namespace DAL.IntegrationTests
+{
+    [TestClass]
+    public class SyntheticGraphDatabaseWriterTests
+    {
+        private TestContext testContextInstance;
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [TestMethod]
+        public void Write_ShouldWriteCorrectGraph()
+        {
+            string currentDir = System.AppDomain.CurrentDomain.BaseDirectory;
+            string filename = string.Format("{0}Resources\\TestDataSet.data", Path.GetFullPath(Path.Combine(currentDir, @"..\..\")));
+
+            SyntheticGraphDatabaseReader reader = new SyntheticGraphDatabaseReader();
+            List<Graph> orignalGraphDb = reader.Read(filename);
+
+            SyntheticGraphDatabaseWriter writer = new SyntheticGraphDatabaseWriter();
+            string tempFile = Guid.NewGuid().ToString();
+            writer.Write(tempFile, orignalGraphDb);
+
+            List<Graph> graphDbAfterWrite = reader.Read(tempFile);
+
+            CollectionAssert.AreEqual(orignalGraphDb, graphDbAfterWrite);
+
+        }
+    }
+}
---
+++ FinalProject/DAL/SyntheticGraphDatabaseWriter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace DAL
+{
+    public class SyntheticGraphDatabaseWriter
+    {
+        public void Write(string filename, List<Graph> graphDb)
+        {
+            using (StreamWriter sw = new StreamWriter(filename))
+            {
+                foreach (Graph graph in graphDb)
+                {
+                    WriteGraph(sw, graph);
+                }
+            }
+        }
+
+        private void WriteGraph(StreamWriter sw, Graph graph)
+        {
+            sw.WriteLine(string.Format("t # {0}", graph.id));
+            sw.Flush();
+
+            WriteGraphVertices(sw, graph.nodes);
+            WriteGraphEdges(sw, graph.edges);
+        }
+
+        private void WriteGraphEdges(StreamWriter sw, List<DFS_Code> graphEdges)
+        {
+            foreach (DFS_Code edge in graphEdges)
+            {
+                sw.WriteLine(string.Format("e {0} {1} {2}", edge.u, edge.v, edge.l_w));
+                sw.Flush();
+            }
+        }
+
+        private void WriteGraphVertices(StreamWriter sw, List<Node> graphNodes)
+        {
+            foreach (Node node in graphNodes)
+            {
+                sw.WriteLine(string.Format("v {0} {1}", node.id, node.label));
+                sw.Flush();
+            }
+        }
+    }
+}
